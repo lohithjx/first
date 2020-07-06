eb14b35922b43619e9a617f897462e13a7eb8e0d
lohith <lohith.warlock@gmail.com>
2020-07-06 12:02:02 +0530

    initial commit

---
+++ components/First.js
@@ -0,0 +1,29 @@
+import gql from "graphql-tag";
+import { Query } from "react-apollo";
+
+export const QUERY = gql`
+	query {
+		Product {
+			StartingPrice
+			ProductName
+			ProductImageLink
+			Id
+			Categoryy
+		}
+	}
+`;
+
+export default function First() {
+	return (
+		<section>
+			<Query query={QUERY} fetchPolicy={"cache-and-network"}>
+				{({ loading, data, error }) => {
+					if (error) {
+						return <div>Error..</div>;
+					}
+					return <div>{JSON.stringify(data)}</div>;
+				}}
+			</Query>
+		</section>
+	);
+}
---
+++ pages/index.js
@@ -0,0 +1,62 @@
+import { useDispatch } from "react-redux";
+import { initializeStore } from "../lib/redux";
+import { initializeApollo } from "../lib/apollo";
+import useInterval from "../lib/useInterval";
+import Layout from "../components/Layout";
+import Clock from "../components/Clock";
+import Counter from "../components/Counter";
+import Submit from "../components/Submit";
+import PostList, { QUERY, allPostsQueryVars } from "../components/First";
+import First from "../components/first";
+
+const IndexPage = () => {
+	// Tick the time every second
+	const dispatch = useDispatch();
+
+	useInterval(() => {
+		dispatch({
+			type: "TICK",
+			light: true,
+			lastUpdate: Date.now()
+		});
+	}, 1000);
+
+	return (
+		<Layout>
+			{/* Redux */}
+			{/* <Clock />
+      <Counter /> */}
+			<hr />
+			{/* Apollo */}
+			{/* <Submit /> */}
+			{/* <PostList /> */}
+			<First />
+		</Layout>
+	);
+};
+
+export async function getStaticProps() {
+	const reduxStore = initializeStore();
+	const apolloClient = initializeApollo();
+	const { dispatch } = reduxStore;
+
+	dispatch({
+		type: "TICK",
+		light: true,
+		lastUpdate: Date.now()
+	});
+
+	await apolloClient.query({
+		query: QUERY
+	});
+
+	return {
+		props: {
+			initialReduxState: reduxStore.getState(),
+			initialApolloState: apolloClient.cache.extract()
+		},
+		unstable_revalidate: 1
+	};
+}
+
+export default IndexPage;
